4b86647dcb3f668028651abc182221eb3f1a5bce
Mr.Wick <johnwick@gmail.com>
2026-03-26 01:11:53 +0100

    Implement Virtio Block Device

--- include/virtio/virtio_blk.h
+++ include/virtio/virtio_blk.h
@@ -3,62 +3,105 @@
 
 #include <stdint.h>
 
-// Legacy MMIO Offsets
-#define VIRTIO_REG_MAGIC 0x000
-#define VIRTIO_REG_VERSION 0x004
-#define VIRTIO_REG_DEVICE_ID 0x008
-#define VIRTIO_REG_QUEUE_SEL 0x030
-#define VIRTIO_REG_QUEUE_NUM_MAX 0x034
-#define VIRTIO_REG_QUEUE_NUM 0x038
-#define VIRTIO_REG_QUEUE_ALIGN 0x03c
-#define VIRTIO_REG_QUEUE_PFN 0x040
-#define VIRTIO_REG_QUEUE_NOTIFY 0x050
-#define VIRTIO_REG_STATUS 0x070
+#include "../mmu.h"
 
-#define MAX_QUEUE_SIZE 128
-#define PAGE_SIZE 4096
+#define SECTOR_SIZE 512
+#define VIRTQ_ENTRY_NUM 16
+#define VIRTIO_DEVICE_BLK 2
+#define VIRTIO_BLK_ADDRESS 0x0A000000
+#define VIRTIO_REG_MAGIC 0x00
+#define VIRTIO_REG_VERSION 0x04
+#define VIRTIO_REG_DEVICE_ID 0x08
+#define VIRTIO_REG_PAGE_SIZE 0x28
+#define VIRTIO_REG_QUEUE_SEL 0x30
+#define VIRTIO_REG_QUEUE_NUM_MAX 0x34
+#define VIRTIO_REG_QUEUE_NUM 0x38
+#define VIRTIO_REG_QUEUE_PFN 0x40
+#define VIRTIO_REG_QUEUE_READY 0x44
+#define VIRTIO_REG_QUEUE_NOTIFY 0x50
+#define VIRTIO_REG_DEVICE_STATUS 0x70
+#define VIRTIO_REG_DEVICE_CONFIG 0x100
+#define VIRTIO_STATUS_ACK 1
+#define VIRTIO_STATUS_DRIVER 2
+#define VIRTIO_STATUS_DRIVER_OK 4
+#define VIRTIO_DESC_F_NEXT 1
+#define VIRTIO_DESC_F_WRITE 2
+#define VIRTIO_AVAIL_F_NO_INTERRUPT 1
+#define VIRTIO_BLK_T_IN 0
+#define VIRTIO_BLK_T_OUT 1
+
+// Result codes
+#define VIRTIO_OK 0
+#define VIRTIO_BAD_MAGIC 1
+#define VIRTIO_UNKNOWN_DEVICE 2
+#define VIRTIO_READ_ERROR 3
 
 struct virtq_desc {
   uint64_t addr;
   uint32_t len;
   uint16_t flags;
   uint16_t next;
-};
+} __attribute__((packed));
 
 struct virtq_avail {
   uint16_t flags;
-  uint16_t idx;
-  uint16_t ring[MAX_QUEUE_SIZE];
-};
+  uint16_t index;
+  uint16_t ring[VIRTQ_ENTRY_NUM];
+} __attribute__((packed));
 
-struct virtq_used_item {
+struct virtq_used_elem {
   uint32_t id;
   uint32_t len;
-};
+} __attribute__((packed));
 
 struct virtq_used {
   uint16_t flags;
-  uint16_t idx;
-  struct virtq_used_item ring[MAX_QUEUE_SIZE];
-};
+  uint16_t index;
+  struct virtq_used_elem ring[VIRTQ_ENTRY_NUM];
+} __attribute__((packed));
 
-typedef struct {
-  struct virtq_desc desc[MAX_QUEUE_SIZE];
+struct virtio_virtq {
+  struct virtq_desc desc[VIRTQ_ENTRY_NUM];
   struct virtq_avail avail;
   struct virtq_used used __attribute__((aligned(4096)));
-} virtq_t __attribute__((aligned(4096)));
+  int queue_index;
+  volatile uint16_t *used_index;
+  uint16_t last_used_index;
+} __attribute__((packed));
 
-typedef struct {
+struct virtio_blk_req {
   uint32_t type;
   uint32_t reserved;
   uint64_t sector;
-} virtio_blk_req_t;
+  uint8_t data[512];
+  uint8_t status;
+} __attribute__((packed));
 
 typedef struct {
-  uintptr_t base;
-  virtq_t *vring;
+  uint64_t base;
+  struct virtio_virtq *vring;
   uint32_t actual_q_size;
   uint16_t last_used_idx;
 } virtio_blk_dev_t;
+
+// MMIO read/write helpers
+static inline uint32_t virtio_read32(uint32_t offset) {
+  return *((volatile uint32_t *)(PA_TO_VA(VIRTIO_BLK_ADDRESS) + offset));
+}
+
+static inline uint64_t virtio_read64(uint32_t offset) {
+  return *((volatile uint64_t *)(PA_TO_VA(VIRTIO_BLK_ADDRESS) + offset));
+}
+
+static inline void virtio_write32(uint32_t offset, uint32_t value) {
+  *((volatile uint32_t *)(PA_TO_VA(VIRTIO_BLK_ADDRESS) + offset)) = value;
+}
+
+static inline void virtio_write64(uint64_t offset, uint64_t value) {
+  *((volatile uint64_t *)(PA_TO_VA(VIRTIO_BLK_ADDRESS) + offset)) = value;
+}
+
+int virtio_blk_init(virtio_blk_dev_t *dev);
+int virtio_blk_read_sector(virtio_blk_dev_t *dev, uint64_t sector, void *buf);
 
 #endif
--- start.sh
+++ start.sh
@@ -7,4 +7,6 @@ SMP=2
 
 qemu-system-aarch64 -M virt,secure=on,gic-version=$GIC \
   -cpu $CPU -smp $SMP -m $RAM -nographic -kernel $IMAGE \
-  -serial mon:stdio -serial file:/dev/fd/2
+  -serial mon:stdio -serial file:/dev/fd/2 \
+  -drive id=drive0,file=data.bin,format=raw,if=none \
+  -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0
